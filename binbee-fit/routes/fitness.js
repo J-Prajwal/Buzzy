c02371085f03985e27276cc8772f913a97245d2b
J-Prajwal <luckyjais058@gmail.com>
2022-09-01 19:35:16 +0530

    BinBee-Fit api done

---
+++ binbee-fit/routes/fitness.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+var express = require("express");
+var router = express.Router();
+require("dotenv").config();
+
+router.get("/", async function (req, res, next) {
+  const result = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  const mes = result.data[0].title;
+  const url = process.env.SLACK_SECRET_HOOK;
+  const slackResult = await axios.post(url, {
+    text: mes,
+  });
+});
+
+module.exports = router;
